8d7eb9e3cd575cd773a1e715cff776fdb1ef49e6
N1ikHard <N1ik@yandex.ru>
2021-04-20 11:32:53 +0300

    Добавил комментарии

--- PasswordBase/Form1.cs
+++ PasswordBase/Form1.cs
@@ -19,16 +19,16 @@ namespace PasswordBase
         }
         private void LogField1_Click(object sender, EventArgs e)
         {
-         Clipboard.SetText((string)((Button)sender).Tag);
+         Clipboard.SetText((string)((Button)sender).Tag);               //Копирование в буфер обмена
         }
 
-        private void LinkField1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
+        private void LinkField1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)     //Переход по ссылке
+        { //На удивление , работает только с IE , при (не)указании другого браузера , выкидывает исключение не нахождения файла
             Process.Start("C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe", (string)((LinkLabel)sender).Tag);
         }
 
         private void PassField1_Click(object sender, EventArgs e)
-        {
+        {//Копирование в буфер обмена пароля и закрытие программы
             Clipboard.SetText((string)((Button)sender).Tag);
             Close();
         }
