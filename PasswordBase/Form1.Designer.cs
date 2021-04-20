3f4b2e918b96ca8d2dbf9f7d02e24cb31d38e14c
N1ikHard <N1ik@yandex.ru>
2021-04-20 11:30:14 +0300

    Добавьте файлы проекта.

---
+++ PasswordBase/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace PasswordBase
+{
+    partial class PasswordForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordForm));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.PassField3 = new System.Windows.Forms.Button();
+            this.LogField3 = new System.Windows.Forms.Button();
+            this.LinkField3 = new System.Windows.Forms.LinkLabel();
+            this.PassField2 = new System.Windows.Forms.Button();
+            this.LogField2 = new System.Windows.Forms.Button();
+            this.LinkField2 = new System.Windows.Forms.LinkLabel();
+            this.PassField1 = new System.Windows.Forms.Button();
+            this.LinkField1 = new System.Windows.Forms.LinkLabel();
+            this.LogField1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.43931F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.03468F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.PassField3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.LogField3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.LinkField3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.PassField2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LogField2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LinkField2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.PassField1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.LinkField1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.LogField1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(346, 108);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // PassField3
+            // 
+            this.PassField3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PassField3.Location = new System.Drawing.Point(241, 75);
+            this.PassField3.Name = "PassField3";
+            this.PassField3.Size = new System.Drawing.Size(94, 29);
+            this.PassField3.TabIndex = 8;
+            this.PassField3.Tag = "563421321";
+            this.PassField3.Text = "password";
+            this.PassField3.UseVisualStyleBackColor = true;
+            this.PassField3.Click += new System.EventHandler(this.PassField1_Click);
+            // 
+            // LogField3
+            // 
+            this.LogField3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LogField3.Location = new System.Drawing.Point(136, 75);
+            this.LogField3.Name = "LogField3";
+            this.LogField3.Size = new System.Drawing.Size(91, 29);
+            this.LogField3.TabIndex = 7;
+            this.LogField3.Tag = "N3ik@yandex.ru";
+            this.LogField3.Text = "login";
+            this.LogField3.UseVisualStyleBackColor = true;
+            this.LogField3.Click += new System.EventHandler(this.LogField1_Click);
+            // 
+            // LinkField3
+            // 
+            this.LinkField3.AutoSize = true;
+            this.LinkField3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LinkField3.Location = new System.Drawing.Point(3, 72);
+            this.LinkField3.Name = "LinkField3";
+            this.LinkField3.Size = new System.Drawing.Size(127, 36);
+            this.LinkField3.TabIndex = 6;
+            this.LinkField3.TabStop = true;
+            this.LinkField3.Tag = "https://www.videosharp.info/";
+            this.LinkField3.Text = "videosharp.info";
+            this.LinkField3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LinkField3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkField1_LinkClicked);
+            // 
+            // PassField2
+            // 
+            this.PassField2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PassField2.Location = new System.Drawing.Point(241, 39);
+            this.PassField2.Name = "PassField2";
+            this.PassField2.Size = new System.Drawing.Size(94, 29);
+            this.PassField2.TabIndex = 5;
+            this.PassField2.Tag = "543123123";
+            this.PassField2.Text = "password";
+            this.PassField2.UseVisualStyleBackColor = true;
+            this.PassField2.Click += new System.EventHandler(this.PassField1_Click);
+            // 
+            // LogField2
+            // 
+            this.LogField2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LogField2.Location = new System.Drawing.Point(136, 39);
+            this.LogField2.Name = "LogField2";
+            this.LogField2.Size = new System.Drawing.Size(91, 29);
+            this.LogField2.TabIndex = 4;
+            this.LogField2.Tag = "N2ik@yandex.ru";
+            this.LogField2.Text = "login";
+            this.LogField2.UseVisualStyleBackColor = true;
+            this.LogField2.Click += new System.EventHandler(this.LogField1_Click);
+            // 
+            // LinkField2
+            // 
+            this.LinkField2.AutoSize = true;
+            this.LinkField2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LinkField2.Location = new System.Drawing.Point(3, 36);
+            this.LinkField2.Name = "LinkField2";
+            this.LinkField2.Size = new System.Drawing.Size(127, 36);
+            this.LinkField2.TabIndex = 3;
+            this.LinkField2.TabStop = true;
+            this.LinkField2.Tag = "https://github.com";
+            this.LinkField2.Text = "github.com";
+            this.LinkField2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LinkField2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkField1_LinkClicked);
+            // 
+            // PassField1
+            // 
+            this.PassField1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PassField1.Location = new System.Drawing.Point(241, 3);
+            this.PassField1.Name = "PassField1";
+            this.PassField1.Size = new System.Drawing.Size(94, 29);
+            this.PassField1.TabIndex = 2;
+            this.PassField1.Tag = "123524123";
+            this.PassField1.Text = "password";
+            this.PassField1.UseVisualStyleBackColor = true;
+            this.PassField1.Click += new System.EventHandler(this.PassField1_Click);
+            // 
+            // LinkField1
+            // 
+            this.LinkField1.AutoSize = true;
+            this.LinkField1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LinkField1.Location = new System.Drawing.Point(3, 0);
+            this.LinkField1.Name = "LinkField1";
+            this.LinkField1.Size = new System.Drawing.Size(127, 36);
+            this.LinkField1.TabIndex = 0;
+            this.LinkField1.TabStop = true;
+            this.LinkField1.Tag = "https://mail.yandex.ru";
+            this.LinkField1.Text = "mail.yandex.ru";
+            this.LinkField1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LinkField1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkField1_LinkClicked);
+            // 
+            // LogField1
+            // 
+            this.LogField1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LogField1.Location = new System.Drawing.Point(136, 3);
+            this.LogField1.Name = "LogField1";
+            this.LogField1.Size = new System.Drawing.Size(91, 29);
+            this.LogField1.TabIndex = 1;
+            this.LogField1.Tag = "N1ik@yandex.ru";
+            this.LogField1.Text = "login";
+            this.LogField1.UseVisualStyleBackColor = true;
+            this.LogField1.Click += new System.EventHandler(this.LogField1_Click);
+            // 
+            // PasswordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 123);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PasswordForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Password";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button PassField3;
+        private System.Windows.Forms.Button LogField3;
+        private System.Windows.Forms.LinkLabel LinkField3;
+        private System.Windows.Forms.Button PassField2;
+        private System.Windows.Forms.Button LogField2;
+        private System.Windows.Forms.LinkLabel LinkField2;
+        private System.Windows.Forms.Button PassField1;
+        private System.Windows.Forms.LinkLabel LinkField1;
+        private System.Windows.Forms.Button LogField1;
+    }
+}
+
---
+++ PasswordBase/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace PasswordBase
+{
+    public partial class PasswordForm : Form
+    {
+        public PasswordForm()
+        {
+            InitializeComponent();
+        }
+        private void LogField1_Click(object sender, EventArgs e)
+        {
+         Clipboard.SetText((string)((Button)sender).Tag);
+        }
+
+        private void LinkField1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe", (string)((LinkLabel)sender).Tag);
+        }
+
+        private void PassField1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText((string)((Button)sender).Tag);
+            Close();
+        }
+    }
+}
